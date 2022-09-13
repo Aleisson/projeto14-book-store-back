934cc498fb4590aa0188d55ea50089248582de08
Aleisson <caioaleisson@gmail.com>
2022-09-13 14:37:12 -0300

    chore: adjust code for deploy on heroku

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import {json} from 'express';
+import dotnev from 'dotenv';
+import cors from 'cors';
+import { MongoClient } from "mongodb";
+
+
+dotnev.config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+
+console.log(process.env.MONGO_URI)
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let database;
+mongoClient.connect(async () => {
+    database = await mongoClient.db("bookstore");
+})
+
+app.listen(process.env.PORT, () =>{
+    console.log("Server running on port " + process.env.PORT);
+});
