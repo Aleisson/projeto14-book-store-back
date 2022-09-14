9d7a0faddb1e7c088518166b5430658091fbc7e5
Aleisson <caioaleisson@gmail.com>
2022-09-14 15:26:45 -0300

    validation schema

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,25 +1,35 @@
 import {STATUS_CODE} from '../enums/statusCode.js';
 import bcrypt from 'bcrypt';
 import database from '../database/database.js'
+import {signUpSchema} from '../schemas/authSchema.js'
 
 async function signUp(req, res){
+
+
+    const isValid = signUpSchema.validate(req.body);
+
+    if(isValid.error){
+        return res.sendStatus(STATUS_CODE.BAD_REQUEST);
+    }
 
     const {name, email, password} = req.body;
 
     const passwordHash = bcrypt.hashSync(password,10);
 
+
+
     try{
 
         const checkUser = await database.collection('users').findOne({email});
         if(checkUser){
-            return res.send(STATUS_CODE.CONFLICT);
+            return res.sendStatus(STATUS_CODE.CONFLICT);
         }
 
         database.collection('users').insertOne({name, email, password: passwordHash});
-        return res.send(STATUS_CODE.CREATED);
+        return res.sendStatus(STATUS_CODE.CREATED);
     }catch(e){
         console.error(error);
-        return res.send(STATUS_CODE.SERVER_ERROR);
+        return res.sendStatus(STATUS_CODE.SERVER_ERROR);
     }
 
     
---
+++ src/schemas/authSchema.js
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+const signUpSchema = Joi.object({
+
+    name: Joi.string().required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
+    password: Joi.string().required(),
+});
+
+export {signUpSchema}
