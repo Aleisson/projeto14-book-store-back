5aa0e906419058624c92534973c88bc730d0c50f
Aleisson <caioaleisson@gmail.com>
2022-09-14 21:10:52 -0300

    products routes

---
+++ src/routes/products.Router.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/products', (res, req) => { return res.setEncoding('<h1> Products </h1>'});
+
+return default router;
