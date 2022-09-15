1d2cca034f3c49349c3e4fa4816423e09f2f721b
Aleisson <caioaleisson@gmail.com>
2022-09-14 21:14:43 -0300

    delete routes products

--- src/routes/products.Router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/products', (res, req) => { return res.setEncoding('<h1> Products </h1>'});
-
-export default router;
