6a35d6fe315a347d62bf05a164f06a6dbead27d0
Aleisson <caioaleisson@gmail.com>
2022-09-14 22:07:09 -0300

    controller products basic update

--- src/controllers/products.Controller.js
+++ src/controllers/products.Controller.js
@@ -1,8 +1,17 @@
+import { STATUS_CODE } from '../enums/statusCode.js'
 
-async function productsGet(req, res){
 
-    return res.send('<h1>Produtos</h2>');
+
+async function productsGet(req, res) {
+
+    const token = req.headers.authorization?.replace('Bearer ', '')
+
+    if (!token) {
+        return res.sendStatus(STATUS_CODE.BAD_REQUEST);
+    }
+
+    return res.send(`<h1>${token}</h2>`);
 
 }
 
-export {productsGet}
+export { productsGet }
